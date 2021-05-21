df46a53510653ce1ed5bd84551a331370c20cae5
Ana Ruiz <ruizana80@gmail.com>
2021-05-20 22:09:09 -0500

    Products with Sockets
    Products with sockets

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,74 @@
-const socket = io.connect();
+const productosForm = document.getElementById('productos-form');
+
+const socket = io();
+
+socket.on('mensaje', (mensaje) => {
+  console.log('hola', mensaje);
+});
+socket.on('products', (prod) => {
+  console.log('desde sock prodc', prod);
+  document.querySelector('.wrapper').innerHTML = '';
+
+  outputProduct(prod);
+});
+
+// Products Submit
+
+productosForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get Products from form
+  const product = {
+    name: e.target.elements.name.value,
+    price: e.target.elements.price.value,
+    thumbnail: e.target.elements.thumbnail.value,
+  };
+
+  // Emit product obejct
+  socket.emit('productForm', product);
+});
+
+function outputProduct(products) {
+  const div = document.createElement('div');
+  div.classList.add('list-group-item');
+  console.log('ldnth', products, products.length);
+  div.innerHTML = products.map(
+    (product) => `<div
+  id='product-list'
+  class='media align-items-lg-center flex-column flex-lg-row p-3'
+>
+  <div class='media-body order-2 order-lg-1'>
+    <h2 class='mt-0 font-weight-bold mb-2'>${product.name}</h2>
+    <div
+      class='d-flex align-items-center justify-content-between mt-1'
+    >
+      <h3 class='font-weight-bold my-2 text-center'>$${product.price} </h3>
+      <ul class='list-inline small'>
+        <li class='list-inline-item m-0'><i
+            class='fa fa-star text-success'
+          ></i></li>
+        <li class='list-inline-item m-0'><i
+            class='fa fa-star text-success'
+          ></i></li>
+        <li class='list-inline-item m-0'><i
+            class='fa fa-star text-success'
+          ></i></li>
+        <li class='list-inline-item m-0'><i
+            class='fa fa-star text-success'
+          ></i></li>
+        <li class='list-inline-item m-0'><i
+            class='fa fa-star-o text-gray'
+          ></i></li>
+      </ul>
+    </div>
+  </div><img
+    src='${product.thumbnail}'
+    alt='Generic placeholder image'
+    width='200'
+    class='ml-lg-5 order-1 order-lg-2'
+  />
+</div>`
+  );
+
+  document.querySelector('.wrapper').appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,46 +1,61 @@
-import express from 'express'
-import {Server as HttpServer} from 'http'
-import {Server as Socket} from 'socket.io'
-import handlebars from 'express-handlebars'
-import frontRoutes from './routes/Front.js'
+import express from 'express';
+import { Server as HttpServer } from 'http';
+import { Server as Socket } from 'socket.io';
+import handlebars from 'express-handlebars';
+import frontRoutes from './routes/Front.js';
 
-import productRouter from './routes/Products.js'
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new Socket(httpServer);
 
-const app = express()
-const httpServer = new HttpServer(app)
-const io = new Socket(httpServer)
+let productDB = [];
 
-io.on('connection', socket => {
-  console.log('cliente conectado!')
-  socket.emit('productos', productos.slice(0, mostrados))
+io.on('connection', (socket) => {
+  // single user  Bienvenido, usurio actual
+  socket.emit('mensaje', 'Bienvenido Usuario actual');
+  socket.emit('products', productDB);
 
-  socket.on('boton', () => {
-    mostrados++
-    socket.emit('productos', productos.slice(0, mostrados))
-  })
-})
+  // socket.emit('product', productDB);
+
+  // Brodcast when a user disconnect- Everybody, except de user
+  socket.broadcast.emit('mensaje', 'Un producto se ha añadido');
+
+  // Cuando un usuario se desconecta
+  socket.on('disconnect', () => {
+    io.emit('mensaje', 'Cliente desconectado');
+  });
+
+  // Escuchar a los productos de la Form
+  socket.on('productForm', (product) => {
+    productDB.push(product);
+    console.log('DB', productDB, productDB.length);
+    io.emit('products', productDB);
+  });
+
+  // Everybody
+  // io.emit()
+});
 
 app.engine(
-    "hbs",
-    handlebars({
-      extname:".hbs",
-      defaultLayout:'index.hbs',
-    })
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+  })
 );
 
-app.set("view engine","hbs");
-app.set("views", "./views");
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/web",frontRoutes)
+app.use('/web', frontRoutes);
 
-
-const PORT = 8080
+const PORT = 8080;
 const server = httpServer.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
